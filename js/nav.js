22c28f8752a9601fe13fd7a989b98d60e54a47a9
pravin86n <pravin86n@gmail.com>
2021-06-02 12:39:22 -0400

    menu fix

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,37 @@ $(window).scroll(function () {
     $(".nav").addClass("position-absolute");
   }
 });
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $(".mobile-navigation-bar").addClass("sticky");
+  } else {
+    $(".mobile-navigation-bar").removeClass("sticky");
+  }
+});
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+
+    //burger animation
+    burger.classList.toggle("cross");
+  });
+};
+
+navSlide();
