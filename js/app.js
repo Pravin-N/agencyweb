5b61fb632262af0515d8e44aef5800aebf1f14fd
pravin86n <pravin86n@gmail.com>
2021-06-09 17:47:58 -0400

    logo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 
-tl.to(".anim-span", 1.2,  {y: "0%", stagger: 0.3});
-tl.to(".cover", 1, {y: "-100%", ease:Power1.easeIn, delay: 0.4});
+tl.to(".anim-span", 1,  {y: "0%", stagger: 0.3});
+tl.to(".cover", 1, {y: "-100%", ease:Power1.easeIn, delay: 0.2});
 tl.to(".intro-text", 0.19, {color: "transparent"}, '-=0.19');
 tl.to(".intro", 0, {display: "none"})
 tl.to(".cover", 0, {display: "none"})
